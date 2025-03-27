9e1e7a2ef075696110a4e94da054b1d15808aaf6
João Pedro <joaopdesousa12@gmail.com>
2025-03-26 22:54:53 -0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import "./App.css";
+import "./Map";
 import React, { useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 function App() {
   const latitude = 51.505;
   const longitude = -0.09;
-  return (
-    <MapContainer
-      className="teste"
-      center={[51.505, -0.09]}
-      zoom={7}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
+  return <Map />;
 }
 
 export default App;
